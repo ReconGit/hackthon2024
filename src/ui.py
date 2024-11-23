6fbc51752b99cff28527854210372fe2b0e60836
Radovan Beler <beler.radovan@gmail.com>
2024-11-23 21:06:02 +0100

    Improve UI

--- src/ui.py
+++ src/ui.py
@@ -1,50 +1,65 @@
 import streamlit as st
+import streamlit_pdf_viewer as stpdf
 
 st.set_page_config(page_title="QuickForm", layout="wide", initial_sidebar_state="expanded")
-
 st.title("QuickForm")
 
-file_column, result_column = st.columns(2)
 
-with file_column:
+def display_result(con, result):
+    for entry in result:
+        c = con.container(border=True)
+        icon_col, error_col = c.columns([0.05, 0.8], vertical_alignment="center")
+        with icon_col:
+            st.image("resources/error.png")
+        with error_col:
+            st.write(entry["message"])
+
+
+if "results" not in st.session_state:
+    st.session_state.results = None
+
+left_col, _, right_col = st.columns([0.35, 0.05, 0.6])
+
+with left_col:
+    results = st.session_state.results
     st.header("Drop here your documents")
     document = st.file_uploader("Document", key="document", type=["txt", "pdf"])
     template = st.file_uploader("Template", key="template", type=["txt", "pdf"])
 
-document_content = None
-if document is not None:
-    file_type = document.type
-    if file_type == "text/plain":
-        document_content = document.getvalue()
-    elif file_type == "application/pdf":
-        document_content = document.getvalue()
+    if st.button("Submit"):
+        st.session_state.results = [
+            {"level": "error", "message": "Incorrect phone number"},
+            {"level": "warning", "message": "Better wording. Try ...."},
+        ]
 
-template_content = None
-if template is not None:
-    file_type = template.type
-    if file_type == "text/plain":
-        template_content = template.getvalue()
-    elif file_type == "application/pdf":
-        template_content = template.getvalue()
+    # st.header("Chat")
 
-data = [
-    {"line": 12, "error": "Phone number has invalid format"},
-    {"line": 24, "error": "some other text"},
-]
+with right_col:
+    results_tab, preview_tab, chat_tab = st.tabs(["Results", "Preview", "Chat"])
+    with results_tab:
+        st.header("Results")
+        results = st.session_state.results
+        if results:
+            display_result(results_tab, results)
+        else:
+            right_col.write("Nothing to see...")
+    with preview_tab:
+        st.header("Preview")
+        stpdf.pdf_viewer("resources/test_pdf.pdf", width=500)
+    with chat_tab:
+        st.header("Chat")
 
-if st.button("Submit"):
-    with result_column:
-        st.header("Result")
-        for d in data:
-            c = st.container(border=True)
-            icon_col, error_col = c.columns([0.05, 0.8], vertical_alignment="center")
-            with icon_col:
-                # st.write("sinep")
-                st.image("error.png")
-            with error_col:
-                st.write(f"Page {d["line"]}")
-                st.write(f"{d["error"]}")
-else:
-    with result_column:
-        st.header("Result")
-        st.write("Nothing to see...")
+
+# if document is not None:
+#     file_type = document.type
+#     if file_type == "text/plain":
+#         document_content = document.getvalue()
+#     elif file_type == "application/pdf":
+#         document_content = document.getvalue()
+
+# if template is not None:
+#     file_type = template.type
+#     if file_type == "text/plain":
+#         template_content = template.getvalue()
+#     elif file_type == "application/pdf":
+#         template_content = template.getvalue()
