0eee0b19ebfa277a17a8bd1bcde2b24f2d34c8c3
Radovan Beler <beler.radovan@gmail.com>
2024-11-24 06:47:01 +0100

    asdklfj

--- src/ui.py
+++ src/ui.py
@@ -11,7 +11,7 @@ if "session_id" not in st.session_state:
 if "results" not in st.session_state:
     st.session_state.results = None
 
-if "chat_hisotry" not in st.session_state:
+if "chat_history" not in st.session_state:
     st.session_state.chat_history = {}
 
 if "doc" not in st.session_state:
@@ -31,35 +31,38 @@ st.title("QuickForm")
 
 def show_icon(level):
     match level:
-        case "error":
+        case "incorrect":
             st.image("resources/error.png")
-        case "warning":
+        case "missing":
+            st.image("resources/error.png")
+        case "improvable":
             st.image("resources/warning.png")
 
 
 def display_result(con, result):
-    i = 0
     for entry in result:
         c = con.container(border=True)
         icon_col, error_col = c.columns([0.05, 0.6], vertical_alignment="center")
         with icon_col:
-            show_icon(entry["level"])
+            show_icon(entry["type"])
         with error_col:
             with st.expander(entry["message"]):
-                st.write("Bdaksljflkajds;f")
-        i += 1
+                st.write(entry["suggestion"])
 
 
 def hightlight_color(defect):
-    if defect["level"] == "error":
+    if defect["type"] == "error":
         return (1, 0, 0)
     return (1, 1, 0)
 
 
 def highlight_defect(pdf, defect):
+    if defect["type"] == "missing":
+        return
+
     for i in range(0, pdf.page_count):
         page = pdf[i]
-        for match in page.search_for(defect["message"]):
+        for match in page.search_for(defect["occurrence"]):
             highlight = page.add_rect_annot(match)
             highlight.set_colors(stroke=hightlight_color(defect))
             highlight.set_border(width=1)
@@ -73,6 +76,7 @@ def highlight_defects():
 
     pdf = pymupdf.open(stream=doc.getvalue(), filetype="pdf")
     for defect in st.session_state.results:
+        print(defect)
         highlight_defect(pdf, defect)
 
     buffer = BytesIO()
@@ -91,7 +95,7 @@ def display_chat(prompt_, message_):
     if not session_id_ in chat_history:
         chat_history[session_id_] = [(prompt_, message_)]
     else:
-        chat_history[session_id_].append((prompt_, message_))
+        chat_history[session_id_].insert(0, (prompt_, message_))
     st.session_state.chat_history = chat_history
 
     for exchange in chat_history[session_id_]:
@@ -105,31 +109,26 @@ left_col, _, right_col = st.columns([0.35, 0.05, 0.6])
 
 with left_col:
     st.header("Upload your documents here")
-    document = st.file_uploader("Document", key="document", type=["pdf"])
     template = st.file_uploader("Template", key="template", type=["pdf"])
-
-    st.session_state.doc = document
-    st.session_state.tpl = template
+    document = st.file_uploader("Document", key="document", type=["pdf"])
 
     if st.button("Submit"):
         if document and template:
+            st.session_state.doc = document
+            st.session_state.tpl = template
+
             files = [
                 ("files", (file.name, file.getvalue(), file.type)) for file in [document, template]
             ]
 
-            session_id_ = st.session_state.session_id
             st.session_state.session_id += 1
-
-            st.session_state.results = [
-                {"level": "error", "message": "Hackathon"},
-                {"level": "warning", "message": '(hereinafter referred to as the "Competition")'},
-            ]
+            session_id_ = st.session_state.session_id
             data = {
                 "session_id": session_id_,
                 "message": "Hello, I need help with this document",
             }
             response = requests.post("http://localhost:8000/analysis", data=data, files=files)
-            print(response.text)
+            st.session_state.results = response.json()["analysis"]["issues"]
 
 with right_col:
     st.header("Result")
