942e8cbb4d9bf4e25ad4494c937d0b2aa98499e7
Radovan Beler <beler.radovan@gmail.com>
2024-11-24 03:49:44 +0100

    Add PDF highlighting

--- src/ui.py
+++ src/ui.py
@@ -1,8 +1,21 @@
-import base64
+from io import BytesIO
 
+import pymupdf
 import requests
 import streamlit as st
 import streamlit_pdf_viewer as stpdf
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = 0
+
+if "results" not in st.session_state:
+    st.session_state.results = None
+
+if "doc" not in st.session_state:
+    st.session_state.doc = None
+
+if "tpl" not in st.session_state:
+    st.session_state.tpl = None
 
 st.set_page_config(
     page_title="QuickForm",
@@ -25,48 +38,84 @@ def display_result(con, result):
     i = 0
     for entry in result:
         c = con.container(border=True)
-        # icon_col, error_col, goto_col = c.columns([0.05, 0.6, 0.1], vertical_alignment="center")
         icon_col, error_col = c.columns([0.05, 0.6], vertical_alignment="center")
         with icon_col:
             show_icon(entry["level"])
         with error_col:
             with st.expander(entry["message"]):
                 st.write("Bdaksljflkajds;f")
-        # with goto_col:
-        #     st.button("Goto", key=f"goto_{i}")
         i += 1
 
 
-if "results" not in st.session_state:
-    st.session_state.results = None
+def hightlight_color(defect):
+    if defect["level"] == "error":
+        return (1, 0, 0)
+    return (1, 1, 0)
+
+
+def highlight_defect(pdf, defect):
+    for i in range(0, pdf.page_count):
+        page = pdf[i]
+        for match in page.search_for(defect["message"]):
+            highlight = page.add_rect_annot(match)
+            highlight.set_colors(stroke=hightlight_color(defect))
+            highlight.set_border(width=1)
+            highlight.update()
+
+
+def highlight_defects():
+    doc = st.session_state.doc
+    if not doc or not st.session_state.results:
+        return
+
+    pdf = pymupdf.open(stream=doc.getvalue(), filetype="pdf")
+    for defect in st.session_state.results:
+        highlight_defect(pdf, defect)
+
+    buffer = BytesIO()
+    pdf.save(buffer)
+    pdf.close()
+    with open("tmp.pdf", mode="wb") as f:
+        f.write(buffer.getbuffer())
+    stpdf.pdf_viewer("tmp.pdf", width=700)
+
 
 left_col, _, right_col = st.columns([0.35, 0.05, 0.6])
 
 with left_col:
-    results = st.session_state.results
     st.header("Upload your documents here")
     document = st.file_uploader("Document", key="document", type=["pdf"])
     template = st.file_uploader("Template", key="template", type=["pdf"])
 
+    st.session_state.doc = document
+    st.session_state.tpl = template
+
     if document and template:
-        files = [("files", (file.name, file.getvalue(), file.type)) for file in [document, template]]
+        files = [
+            ("files", (file.name, file.getvalue(), file.type)) for file in [document, template]
+        ]
 
     if st.button("Submit"):
         if document and template:
+            session_id = st.session_state.session_id
+            st.session_state.session_id += 1
+
             st.session_state.results = [
-                {"level": "error", "message": "Incorrect phone number"},
-                {"level": "warning", "message": "Better wording. Try ...."},
+                {"level": "error", "message": "Hackathon"},
+                {"level": "warning", "message": '(hereinafter referred to as the "Competition")'},
             ]
             data = {
-                "session_id": 1,
+                "session_id": session_id,
                 "message": "Hello, I need help with this document",
             }
-            response = requests.post("http://localhost:8000/structure", data=data, files=files)
-            print(response.text)
+            r = requests.post("http://localhost:8000/structure", data=data, files=files)
+            print(r.text)
 
 with right_col:
     st.header("Result")
-    defects_tab, overview_tab, preview_tab, chat_tab = st.tabs(["Defects", "Overview", "Preview", "Chat"])
+    defects_tab, overview_tab, preview_tab, chat_tab = st.tabs(
+        ["Defects", "Overview", "Preview", "Chat"]
+    )
     with defects_tab:
         results = st.session_state.results
         if results:
@@ -76,8 +125,10 @@ with right_col:
     with overview_tab:
         st.write("")
     with preview_tab:
-        stpdf.pdf_viewer("resources/test_pdf.pdf", width=500)
+        highlight_defects()
     with chat_tab:
         prompt = st.chat_input("Ask something about document")
         if prompt:
-            st.write(f"Prompt: {prompt}")
+            session_id = st.session_state.session_id
+            data = {"session_id": session_id, "prompt": prompt}
+            r = requests.post("http://localhost:8000/chat", json=data)
